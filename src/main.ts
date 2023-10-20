6bdef70f806ffab0acfcf5d321be6022073fb37b
poma <poma6185@gmail.com>
2023-10-20 01:41:30 -0400

    ejemplo

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,13 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+/**===================================================
+ * configurar el archivo vue.config.js, para un deploy personalizado
+ * 
+ * deploy en netfily
+ * 
+ * 
+ * 
+ *
+ ===================================================*/
